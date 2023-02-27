a26fddace3a1c77d09a31dbb8185327b64584ce7
Annie-Mai <anniemai025@gmail.com>
2023-02-26 21:33:38 -0500

    added navbar

---
+++ farm-animal/src/components/App.js
@@ -0,0 +1,30 @@
+import "../styles/App.css"
+import Navbar from "./navbar/Navbar";
+import Info from "./navbar/Info";
+import Contact from "./navbar/Contact";
+import Leaderboard from "./navbar/Leaderboard";
+import Login from "./navbar/Login";
+import Register from "./navbar/Register";
+import { Route, Routes } from "react-router-dom";
+import Home from "./navbar/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/info"element={<Info />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ farm-animal/src/components/navbar/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Background from "../Background";
+import '../../styles/navbar_/Home.css';
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <Background />
+    </div>
+  )
+}
+
+export default Home
