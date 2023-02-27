af37aa1a2b6abe978352e3a0b3be310d830e6fa7
Chloemz <57193454+Chloemz@users.noreply.github.com>
2023-02-26 19:33:54 -0500

    added Homepage gif and changed background color app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import {
-  BrowserRouter as Router, 
-  Switch,
-  Route
-} from 'react-router-dom'
+import logo from './assets/HomeImage.gif'
 import Layout from './components/Layout'
 import HomePage from './components/HomePage'
 import About from './components/About'
@@ -14,7 +10,24 @@ import Contact from './components/Contact'
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <header className="App-header">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        Hello world!
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      
     </div>
     /*<div className="App">
       <header className="App-header">
