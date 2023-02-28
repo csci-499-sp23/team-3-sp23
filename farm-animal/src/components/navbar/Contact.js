8f95bf56b575c29c2c4d722c2230db8c276d30f4
Daniel Lin <lindanny9721@gmail.com>
2023-02-28 09:32:56 -0500

    Changed css for Contact

--- farm-animal/src/components/navbar/Contact.js
+++ farm-animal/src/components/navbar/Contact.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import "../../styles/navbar_/Contact.css";
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      <h1>Contact Us!</h1>
+      <form className = 'contact-form' action = "https://formsubmit.co/emailhere@gmail.com" method='POST'>
+        <label className= 'contact-label' htmlFor = "name">Name: </label>
+        <input className = 'contact-input' type = "name" placeholder='Name...' id = "name"/>
+        <label className= 'contact-label' htmlFor = "email">Email: </label>
+        <input className = 'contact-input' type = "email" placeholder='Email...' id = "email"/>
+        <label className= 'contact-label' htmlFor = "name">Phone: </label>
+        <input className = 'contact-input' type = "phone" placeholder='+1 (123) 456-7890' id = "phone"/>
+        <label className= 'contact-label' htmlFor = "name">Comment: </label>
+        <textarea className = 'contact-text' type = "comment" placeholder='Comments/Suggestions' id = "comment"/>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
