b92c15fbb070fbb90b10bbc2e813b5e4cf6cc457
Daniel Lin <lindanny9721@gmail.com>
2023-02-28 09:02:11 -0500

    Changed css for login

--- farm-animal/src/components/navbar/Login.js
+++ farm-animal/src/components/navbar/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import "../../styles/navbar_/Login.css";
 import { Link } from "react-router-dom"
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -14,15 +15,20 @@ const Login = () => {
     }
   }
   return (
-    <div className=''>
-      <form onSubmit={handleSubmit}>
+    <div className='login-container'>
+      <form className = 'login-form' onSubmit={handleSubmit}>
+        <h2>Account Login</h2>
         <label htmlFor = "email">Email: </label>
-        <input type = "email" placeholder='Players Email' id = "email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input className = "login-input" type = "email" placeholder='Players Email' id = "email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <label htmlFor = "password"> Password:</label>
-        <input type = "password" placeholder='Players Password' id = "password" name= "passowrd" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <input className = "login-input" type = "password" placeholder='Players Password' id = "password" name= "passowrd" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <div >
+          <Link to = "/forgetUser" className='login-link'>Forget User</Link>
+          <Link to = "/forgetPassword">Forget Password</Link>
+        </div>
         <button type = "submit">Login Now!</button>
+        <Link to = "/Register">Register Here!</Link>
       </form>
-      <Link to = "/Register" className='test'>Register Here!</Link>
     </div>
   )
 }
