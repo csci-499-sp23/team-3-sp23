afcdc37bb1b6ea9c6e7b6e779ddb6e9d1dc2c749
Shermol <shermol.peters01@gmail.com>
2023-02-28 08:57:31 -0500

    info page

--- farm-animal/src/components/navbar/Info.js
+++ farm-animal/src/components/navbar/Info.js
@@ -1,12 +1,19 @@
 import React from 'react'
-//import info from '/Users/sherm/Desktop/Capstone/team-3-sp23/farm-animal/src/assets/App_Info.jpg'
+import '../../styles/Info.css';
 
 const Info = () => {
   return (
-    <div>
-    {/* <img src={info} 
-    alt="Info" width="95%" height="95%" /> */}
-  </div>
+    <div className='info'> 
+      <p>
+        Animal Mafia is a free turn based game set in your
+        own farm. Users would be given the chance to 
+        compete against other members of the Mob to gain the chance 
+        of becoming the top Don. Animal Mafia allows for users to 
+        immerse themselves into the game and build a story of their own. 
+        Click play to start your adventure!!!
+      </p>
+    </div>
+
   )
 }
 
