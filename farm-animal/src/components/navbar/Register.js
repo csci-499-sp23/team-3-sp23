ee397e235f130afb225d9e45f3015f51852e5977
Chloemz <57193454+Chloemz@users.noreply.github.com>
2023-02-28 05:41:10 -0500

    fixed Login button on Register component

--- farm-animal/src/components/navbar/Register.js
+++ farm-animal/src/components/navbar/Register.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import "../../styles/navbar_/Register.css";
 import axios from 'axios';
+import { Link } from "react-router-dom"
 
 
 const Register = (props) => {
@@ -31,16 +32,36 @@ const Register = (props) => {
         <label htmlFor="password">Password</label>
         <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="enter password"></input>
         <button type ="submit">Register</button>
-        <a href="/login">
-        <button className="link-button" >Login</button>
-        </a>
+        <Link to ="/Login" className="link-button">
+        <button  >Login</button>
+        </Link>
+        {/* <a href="/login">
+        
+        </a> */}
       </form>
       
       
       {/* <button className="link-button" onClick={() => props.onFormSwitch('login')}>Sign in</button> */}
 
     </div>
-     
+    /*
+    <div>Register</div>
+
+    <div className="register-container">
+      <form className="register-form"onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input value={username} onChange={(e)=> setUsername(e.target.value)}type="username" placeholder="create username"></input>
+
+        <label htmlFor="email">Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="your email"></input>
+        <label htmlFor="password">Password</label>
+        <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="enter password"></input>
+        <button type ="submit">Register</button>
+        <a href="/login">
+        <button className="link-button" >Login</button>
+        </a>
+      </form>
+    */  
   )
 }
 
