93c6527ce9e58eb6f32f64fca00b895b3c58d18a
Shermol <shermol.peters01@gmail.com>
2023-02-28 08:24:22 -0500

    info

--- farm-animal/src/components/navbar/Info.js
+++ farm-animal/src/components/navbar/Info.js
@@ -1,8 +1,12 @@
 import React from 'react'
+//import info from '/Users/sherm/Desktop/Capstone/team-3-sp23/farm-animal/src/assets/App_Info.jpg'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div>
+    {/* <img src={info} 
+    alt="Info" width="95%" height="95%" /> */}
+  </div>
   )
 }
 
