e9ff5e97b5f136b98d7a04fffc44fc18a167e94c
chloealbright <57193454+chloealbright@users.noreply.github.com>
2023-02-28 06:39:32 -0500

    update: deleted comments

--- farm-animal/src/components/navbar/Register.js
+++ farm-animal/src/components/navbar/Register.js
@@ -35,33 +35,13 @@ const Register = (props) => {
         <Link to ="/Login" className="link-button">
         <button  >Login</button>
         </Link>
-        {/* <a href="/login">
-        
-        </a> */}
+       
       </form>
       
       
-      {/* <button className="link-button" onClick={() => props.onFormSwitch('login')}>Sign in</button> */}
 
     </div>
-    /*
-    <div>Register</div>
-
-    <div className="register-container">
-      <form className="register-form"onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input value={username} onChange={(e)=> setUsername(e.target.value)}type="username" placeholder="create username"></input>
-
-        <label htmlFor="email">Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="your email"></input>
-        <label htmlFor="password">Password</label>
-        <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="enter password"></input>
-        <button type ="submit">Register</button>
-        <a href="/login">
-        <button className="link-button" >Login</button>
-        </a>
-      </form>
-    */  
+    
   )
 }
 
