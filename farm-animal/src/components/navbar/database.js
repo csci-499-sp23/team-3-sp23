35ffaed1ab9c91edb37524c8464748eb2eab2a90
Liza <62193040+LizaFeng@users.noreply.github.com>
2023-03-02 19:27:17 -0500

    moving database

---
+++ farm-animal/src/components/navbar/database.js
@@ -0,0 +1,67 @@
+export const scores = [
+  {
+    name: "appleTurkey",
+    location: "USA",
+    score: 4770,
+    img: "../assets/turkey.png",
+    dt: "2022-02-10"
+  },
+  {
+    name: "wheatBread",
+    location: "Poland",
+    score: 2009,
+    img: "../assets/bread.png",
+    dt: "2021-01-01"
+  },
+  {
+    name: "eggFlower",
+    location: "Phillippines",
+    score: 1503,
+    img: "../assets/egg.png",
+    dt: "2021-08-17"
+  },
+  {
+    name: "Pan da",
+    location: "China",
+    score: 1400,
+    img: "../assets/panda.png",
+    dt: "2021-10-23"
+  },
+  {
+    name: "floodDuck",
+    location: "Vietnam",
+    score: 3886,
+    img: "../assets/duck.png",
+    dt: "2022-01-22"
+  },
+  {
+    name: "messier81",
+    location: "Botswana",
+    score: 4109,
+    img: "../assets/messy",
+    dt: "2022-01-21"
+  },
+  {
+    name: "sweetPotato",
+    location: "Liechtenstein",
+    score: 3185,
+    img: "../assets/spotato.png",
+    dt: "2022-07-21"
+  },
+  {
+    name: "puddingLime",
+    location: "Cyprus",
+    score: 2652,
+    img: "../assets/pudding.png",
+    dt: "2022-01-11"
+  },
+  {
+    name: "cucumberPig",
+    location: "Liberia",
+    score: 3398,
+    img: "../assets/pig.png",
+    dt: "2022-10-05"
+  }
+
+]
+//Move to backend later on
