02875e046002012ee0cc657884e04375896f736d
Shermol <shermol.peters01@gmail.com>
2023-03-07 15:22:43 -0500

    changes to info page

--- farm-animal/src/components/navbar/Info.js
+++ farm-animal/src/components/navbar/Info.js
@@ -4,32 +4,32 @@ import '../../styles/Info.css';
 const Info = () => {
   return (
     <div className='entire-page'>
+
       <div className= "about-section"> 
         <h1>About us Page</h1>
         <p> 
-        Animal Mafia is the mixture of a Dungeon Explorer Game and AFK farm simulator (revise later). 
-        Get immersed into an animated world where you play the role of a farm animal lost in the ranks of the mob. 
-        Tying to make a name for yourself, you explore the world of southern New Jersey in hopes of one day becoming the “Don”. 
-        Making new fiends and foes along the way, Animal mafia is a great way to spend your day. 
-        Play alone or compete against friends in the race to become the top of your mob 
-        Click play to start your adventure!!!
+        Get immersed into an animated world of farm animals trying to level up in a kill or be killed world of Animal Mafia. Trying to join the ranks of the mafia, you'll explore an animal farm in southern New Jersey in hopes of one day becoming “The Don”. But it won't come easy while making new enemies along the way. Play alone or compete against friends in the race to become the top of your mob 
+        </p>
+        <br></br>
+        <p>
+        Animal Mafia's game play is similar to Dungeon Explorer in terms of game layout and battle play. The farming portion of this game (similar to AFK) allows users to win seeds from battles that they can use to plant and grow crops to level up their animals. Each user starts out with a randomly generated farm animal with entry level stats and receives a small loan of 500 points to get them started in the game. The Don expects their money in full, therefore 500 points will automatically deduct from the user's wallet when their animal reaches 1000 points in profit (a small price to play to level up). Secured by Blockchain, this point system will allows users to transfer or sell points to other users registered in Animal Mafia. Conceptually this currency can be used in other games made by the same developers. In a scenario where the developers created a new game, veteran gamers of Animal Mafia can transfer their points to another game or sell it securely to other registered users. 
         </p>
       </div>
 
-      <h1 classname = "meet-team-css">Meet the Team</h1>
+      <h1 className='meet-the-team'>Meet the Team</h1>
 
       <div className='row'>
         <div className="column">
           <div className='card'>
             <div className='container'>
               <h2>Shermol</h2>
-              <p className='title'>
+              <p className='card-title'>
                 Useless Team Member
               </p>
-              <p>
+              <p className='card-body'>
                 Shermol helps out sometimes
               </p>
-              <p>
+              <p className='card-email'>
                 shermolpeters@gmail.com
               </p>
             </div>  
@@ -41,13 +41,13 @@ const Info = () => {
             {/* <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"> */}
             <div className='container'>
               <h2>Chloe</h2>
-              <p className='title'>
+              <p className='card-title'>
                 Da Project Owner
               </p>
-              <p>
+              <p className='card-body'>
                 Chloe helps out sometimes
               </p>
-              <p>
+              <p className='card-email'>
                 ChloeAlrbright@gmail.com
               </p>
             </div>  
@@ -59,13 +59,13 @@ const Info = () => {
             {/* <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"> */}
             <div className='container'>
               <h2>Annie</h2>
-              <p className='title'>
+              <p className='card-title'>
                 *insert frog noise*
               </p>
-              <p>
+              <p className='card-body'>
                 Annie helps out sometimes
               </p>
-              <p>
+              <p className='card-email'>
                 AnnieMai@gmail.com
               </p>
             </div>  
@@ -77,13 +77,13 @@ const Info = () => {
             {/* <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"> */}
             <div className='container'>
               <h2>Daniel</h2>
-              <p className='title'>
+              <p className='card-title'>
                 Nightowl aka the sourpatch kid
               </p>
-              <p>
+              <p className='card-body'>
                 Daniel helps out sometimes
               </p>
-              <p>
+              <p className='card-email'>
                 DanielLastName@gmail.com
               </p>
             </div>  
@@ -95,13 +95,13 @@ const Info = () => {
             {/* <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"> */}
             <div className='container'>
               <h2>Liza</h2>
-              <p className='title'>
+              <p className='card-title'>
                 Team Ace
               </p>
-              <p>
+              <p className='card-body'>
                 Liza helps out sometimes
               </p>
-              <p>
+              <p className='card-email'>
                 LizaLastName@gmail.com
               </p>
             </div>  
