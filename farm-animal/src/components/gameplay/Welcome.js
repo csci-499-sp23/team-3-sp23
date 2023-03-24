b8f85dcd14291b2d33ec33932847afa9f1b19b5a
Liza Feng <liza.feng05@myhunter.cuny.edu>
2023-03-24 11:56:42 -0400

    problem: trouble defining sprite class because 'context' is in Canvas.js useEffect but we need context for Sprite too. And we cant import from within useEffect

--- farm-animal/src/components/gameplay/Welcome.js
+++ farm-animal/src/components/gameplay/Welcome.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import Canvas from './Canvas';
+
+const homeBase = new Image();
+homeBase.src = "../src/assets/backgImg/homeBase.png";
+
+//Put the offset of the image here (ie if you want to ste the player to 
+//middle of the image then there would be an offset since it usually starts
+// at (0,0)).
+const offset = {
+  x: -735,
+  y: -650
+}
+
+class Sprite {
+
+}
+
+function draw() {
+  window.requestAnimationFrame(draw)
+  console.log('animate');
+  background.draw();
+}
+
+const background = new Sprite({
+
+  position: {
+    x: offset.x,
+    y: offset.y
+  },
+  image: homeBase
+})
+
+const movables = [background];
+
+// const draw = context => {
+//   //Code to draw an image
+
+// }
 
 function Welcome() {
   return (
-    <div className='message'>Welcome to the game LADILADIDAH</div>
+    <Canvas draw={draw} height={1040} width={576} />
   )
 }
 
