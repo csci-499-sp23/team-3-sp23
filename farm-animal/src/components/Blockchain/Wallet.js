e1110b37ebbe7bf7326e01138bf4c8b99e1358fa
chloealbright <57193454+chloealbright@users.noreply.github.com>
2023-04-04 23:49:30 -0400

    Blockchain folder should be added to components folder

---
+++ farm-animal/src/components/Blockchain/Wallet.js
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import Blockchain from './Blockchain'
+
+
+// export const Wallet = (user) => {
+
+//     const[user, setUser]= useState('')
+//     const[balance, setBalance]= useState(0)
+//     let cannoli = new Blockchain();
+    
+//     const createWallet =()=>{
+
+
+//     }
+
+    
+//     return 
+//     this.user = user;
+//     this.blockchain = new Blockchain();
+
+//     function newWallet
+
+//   return (
+//     <div>Wallet</div>
+//   )
+// }
