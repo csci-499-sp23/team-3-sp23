1ef64799436496880e7483edd9de20c6a9366833
Liza Feng <liza.feng05@myhunter.cuny.edu>
2023-04-13 12:29:33 -0400

    Incorporated unity library. Next steps: figuring out how to attach the game and have it show up

--- farm-animal/src/components/gameplay/Welcome.js
+++ farm-animal/src/components/gameplay/Welcome.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Fragment } from "react"
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 
 function Welcome() {
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+  });
 
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
-
-  )
+    <Fragment>
+      {!isLoaded && (
+        <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+      )}
+      <Unity
+        unityProvider={unityProvider}
+        style={{ visibility: isLoaded ? "visible" : "hidden" }}
+      />
+    </Fragment>
+  );
 }
 
 export default Welcome
